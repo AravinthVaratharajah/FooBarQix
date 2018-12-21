54eb1e1ea7a182a29fa9620618cba98fcd31ba56
Unknown <aravinth.varatharajah@yahoo.com>
2018-12-21 16:41:44 +0100

    #4 - ShouldReturnFooWhenIsDivisibleByThree : Test OK

---
+++ FooBarQix/FooBarQix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FooBarQixKata
+{
+    public class FooBarQix
+    {
+        static void Main(string[] args)
+        {
+            var message = "";
+
+            for (int i = 1; i < 101; i++)
+            {
+                message = IsDivisibleByThree(message, i);
+                CheckMessageEmpty(message, i);
+                message = "";
+            }
+        }
+
+        public static string IsDivisibleByThree(string message, int i)
+        {
+            if (i % 3 == 0)
+                message += "Foo";
+            return message;
+        }
+
+        public static void CheckMessageEmpty(string message, int i)
+        {
+            if (string.IsNullOrEmpty(message))
+                Console.WriteLine(i);
+            else
+                Console.WriteLine(message);
+        }
+    }
+}
--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -1,13 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FooBarQixKata;
 
 namespace UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnFooWhenIsDivisibleByThree()
         {
+            var message = FooBarQix.IsDivisibleByThree("", 3);
+            Assert.AreEqual(message, "Foo");
         }
     }
 }
