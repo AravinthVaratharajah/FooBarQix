26aeb3461047980bf26a945e0933b46bb44cbd3a
Unknown <aravinth.varatharajah@yahoo.com>
2019-01-02 16:00:54 +0100

    #10 - ShouldReturnFooWhenDigitIsThree (Bar -> Five and Qix -> Seven) KO

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -26,5 +26,26 @@ namespace UnitTest
             var message = FooBarQix.IsDivisible(string.Empty, 7);
             Assert.AreEqual(message, "Qix");
         }
+
+        [TestMethod]
+        public void ShouldReturnFooWhenDigitIsThree()
+        {
+            var message = FooBarQix.Contains(string.Empty, 3);
+            Assert.AreEqual(message, "Foo");
+        }
+
+        [TestMethod]
+        public void ShouldReturnFooWhenDigitIsFive()
+        {
+            var message = FooBarQix.Contains(string.Empty, 5);
+            Assert.AreEqual(message, "Bar");
+        }
+
+        [TestMethod]
+        public void ShouldReturnFooWhenDigitIsSeven()
+        {
+            var message = FooBarQix.Contains(string.Empty, 7);
+            Assert.AreEqual(message, "Qix");
+        }
     }
 }
