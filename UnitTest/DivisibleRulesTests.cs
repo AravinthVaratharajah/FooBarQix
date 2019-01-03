49981bd7612f101ceaa5eb8adbe5c86a6f485848
Unknown <aravinth.varatharajah@yahoo.com>
2019-01-03 11:47:34 +0100

    #15 - Refacto Unit Test

---
+++ UnitTest/DivisibleRulesTests.cs
@@ -0,0 +1,34 @@
+﻿using FooBarQixKata;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    public class EmptyClass
+    {
+        public IRules DivisibleRulesTests { get; set; }
+
+        [Test]
+        public void Should_Return_Number_When_Not_In_FooBarQix_Rules()
+        {
+
+        }
+
+        [Test]
+        public void Should_Return_Foo_When_Number_Is_Divisible_By_Three()
+        {
+
+        }
+
+        [Test]
+        public void Should_Return_Bar_When_Number_Is_Divisible_By_Five()
+        {
+
+        }
+
+        [Test]
+        public void Should_Return_Qix_When_Number_Is_Divisible_By_Seven()
+        {
+
+        }
+    }
+}
