02154131ef6126412b39c901a5b990a2ff4c85dc
Unknown <aravinth.varatharajah@yahoo.com>
2019-01-03 14:01:54 +0100

    #17 - Content Rules Tests : KO

---
+++ UnitTest/ContentRulesTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using FooBarQixKata;
+using NFluent;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class ContentRulesTests
+    {
+        public IRules Contener { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Contener = new ContentRules();
+        }
+
+        [TestCase(1, "")]
+        [TestCase(2, "")]
+        [TestCase(11, "")]
+        [TestCase(24, "")]
+        public void Should_Return_Empty_When_Doesnt_Contain_Three_Five_Seven(int number, string expected)
+        {
+
+        }
+
+        [TestCase(3, "Foo")]
+        [TestCase(33, "FooFoo")]
+        public void Should_Return_Foo_When_Number_Contains_Three(int number, string expected)
+        {
+
+        }
+
+        [TestCase(5, "Bar")]
+        [TestCase(55, "BarBar")]
+        public void Should_Return_Bar_When_Number_Contains_Five(int number, string expected)
+        {
+
+        }
+
+        [TestCase(7, "Qix")]
+        [TestCase(77, "QixQix")]
+        public void Should_Return_Qix_When_Number_Contains_Seven(int number, string expected)
+        {
+
+        }
+    }
+}
